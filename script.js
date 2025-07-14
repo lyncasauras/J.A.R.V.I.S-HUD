0a5cebf9078807fe35fbb87c1c2b703f395046a0
lyncasauras <lynclansom@gmail.com>
2025-07-15 06:33:51 +1000

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function launchJarvis() {
-  alert("Launching JARVIS...");
-}
-
-function launchIronKid() {
-  alert("Activating Iron Kid Suit...");
-  document.getElementById('ironKidSection').style.display = 'block';
-}
