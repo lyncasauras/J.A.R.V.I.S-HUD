8c08ff126d01377f783a52e52f943478ca1fbb6b
lyncasauras <lynclansom@gmail.com>
2025-07-15 06:36:06 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function launchJARVIS() {
+  alert("Launching JARVIS...");
+  document.getElementById("powerupSound").play();
+}
+
+function activateIronKid() {
+  alert("Launching Project Iron Kid...");
+  document.getElementById("powerupSound").play();
+}
