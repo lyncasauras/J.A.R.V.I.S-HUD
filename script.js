2248907f36dc76361b3fb1eb03070cc7ff122468
lyncasauras <lynclansom@gmail.com>
2025-07-15 18:04:43 +1000

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-window.addEventListener('DOMContentLoaded', () => {
-  const greeting = document.getElementById('greeting');
-  const msg = new SpeechSynthesisUtterance("Greetings. I am JARVIS, your personal assistant. All systems online.");
-  msg.voice = speechSynthesis.getVoices().find(voice => voice.name.includes("Daniel") || voice.name.includes("Male"));
-  msg.pitch = 1;
-  msg.rate = 1;
-  speechSynthesis.speak(msg);
-});
